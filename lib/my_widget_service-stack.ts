14793d4cac3ec0209e68d641d2034ad017ddaf9d
Tianle Huang <tianleh@amazon.com>
2022-02-19 20:53:17 +0000

    config lambda cdk
    Signed-off-by: Tianle Huang <tianleh@amazon.com>

---
+++ lib/my_widget_service-stack.ts
@@ -0,0 +1,19 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as widget_service from '../lib/widget_service';
+
+export class MyWidgetServiceStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    new widget_service.WidgetService(this, 'Widgets');
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'MyWidgetServiceQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
